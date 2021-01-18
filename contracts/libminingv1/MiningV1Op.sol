b887283cfba28371d9198750791d461395b75350
paradox <paradox@nestprotocol.org>
2021-01-18 21:42:56 +0800

    fix: upadte bitetoken

--- contracts/libminingv1/MiningV1Op.sol
+++ contracts/libminingv1/MiningV1Op.sol
@@ -92,14 +92,15 @@ library MiningV1Op {
 
                 _C_NestPool.freezeNest(address(msg.sender), _newNestNum1k.mul(1000 * 1e18));
 
-                if(newTokenAmountPerEth < uint256(_sheet.tokenAmountPerEth) && _level > state.maxBiteNestedLevel) {
+
+                if( _newEthNum.mul(newTokenAmountPerEth) < biteNum * _sheet.tokenAmountPerEth) {
                     uint256 _unfreezetokenAmount;
-                    _unfreezetokenAmount = uint256(_sheet.tokenAmountPerEth).sub(uint256(newTokenAmountPerEth)).mul(biteNum);
-                    //_C_NestPool.unfreezeToken(address(_sheet.miner), token, _unfreezetokenAmount);
+                    //_unfreezetokenAmount = uint256(_sheet.tokenAmountPerEth).sub(uint256(newTokenAmountPerEth)).mul(biteNum);
+                    _unfreezetokenAmount = uint256(_sheet.tokenAmountPerEth).mul(biteNum).sub((uint256(newTokenAmountPerEth)).mul(_newEthNum));               
                     _C_NestPool.unfreezeToken(msg.sender, token, _unfreezetokenAmount);
                     _C_NestPool.freezeEth(msg.sender, _newEthNum.add(biteNum).mul(1 ether));
                 } else {
-                        _C_NestPool.freezeEthAndToken(msg.sender, _newEthNum.add(biteNum).mul(1 ether), 
+                    _C_NestPool.freezeEthAndToken(msg.sender, _newEthNum.add(biteNum).mul(1 ether), 
                         token, _newEthNum.mul(newTokenAmountPerEth)
                                          .sub(biteNum * _sheet.tokenAmountPerEth));
                 }
